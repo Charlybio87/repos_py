76b66998fef661a83944a96bd1a60250ef2a83de
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-09-29 17:10:45 -0300

    analisis_fc_print()

--- Visual/Cisco/Modulo 2/HolaMundov1.0.py
+++ Visual/Cisco/Modulo 2/HolaMundov1.0.py
@@ -7,17 +7,44 @@
 
 #---Comentario de bloque--- (este coment. no se visualiza en documentacion)
 # Realizando Seguimiento del Modulo 2 
+# Función print()
 
 #---Comentario de Linea---
-print("¡Hola Mundo!");
-print("La Witsi Witsi Araña subió a su telaraña.")
+print("1.¡Hola Mundo!") # Una cadena como el argumento de la función print()
+
+print("2.La Witsi Witsi Araña subió a su telaraña.") # Instrucciones
+print("Vino la lluvia y se la llevó.")
+
+print("3.La Witsi Witsi Araña subió a su telaraña.")
 print()
 print("Vino la lluvia y se la llevó.")
 
 
-print("La Witsi Witsi Araña\nsubió a su telaraña.")
-print("Vino la lluvia\ny se la llevó.")
+print("4.La Witsi Witsi Araña\nsubió a su telaraña.") #Caracteres de escape(\n) y 
+print()                                             #nueva línea en Python
+print("Vino la lluvia\ny se la llevó.") 
 
-print("La Witsi Witsi Araña\nsubió a su telaraña.")
-print("Vino la lluvia\ny se la llevó.")
+print("5.La Witsi Witsi Araña subió a su telaraña.")
+print() # \m \l \o \p \a no realizan nada.
+print("Vino la lluvia y se la llevó.")
+
+print("6.La Witsi Witsi Araña" , "subió" , "a su telaraña.") #Usando múltiples argumentos (x3)
+
+print("7.Mi nombre es", "Python.") #Argumentos posicionales
+print("Monty Python.")
+
+print("8.Mi nombre es", "Python.", end="") #Argumentos de palabras clave
+print("Monty Python.")                      #end=" ",sep="-",
+print("Mi nombre es ", end="  ")
+print("Monty Python.")
+print("Mi nombre es ", end="        ") # Se visualiza un espacio mucho mayor 
+print("Monty Python.")                 # entre ambos argumentos
+
+print("9.Mi nombre es ", end="\n")
+print("Monty Python.")
+
+print("10.Mi", "nombre", "es", "Monty", "Python.", sep="-") #separacion de c/argumento con -
+
+print("11.Mi", "nombre", "es", sep="_", end="*") # Palabra clave pueden mezclarse 
+print("Monty", "Python.", sep="*", end="*\n")    # en una invocación
 
