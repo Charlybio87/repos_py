5066b0ec3d149847c2cb8b10a95958fffe199bc6
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-10-03 08:15:24 -0300

    print()
    elementos para print

--- Visual/Cisco/Modulo 2/HolaMundov1.0.py
+++ Visual/Cisco/Modulo 2/HolaMundov1.0.py
@@ -2,7 +2,7 @@
 #CISCO_PYTHON 1
 #Programa: "Primeros pasos de Python"
 #@author: Ribas Carlos
-#@version 1.0
+#@version 1.0.1
 #@see: """
 
 #---Comentario de bloque--- (este coment. no se visualiza en documentacion)
@@ -21,7 +21,7 @@ print("Vino la lluvia y se la llevó.")
 
 
 print("4.La Witsi Witsi Araña\nsubió a su telaraña.") #Caracteres de escape(\n) y 
-print()                                             #nueva línea en Python
+print()                                               #nueva línea en Python
 print("Vino la lluvia\ny se la llevó.") 
 
 print("5.La Witsi Witsi Araña subió a su telaraña.")
@@ -40,7 +40,7 @@ print("Monty Python.")
 print("Mi nombre es ", end="        ") # Se visualiza un espacio mucho mayor 
 print("Monty Python.")                 # entre ambos argumentos
 
-print("9.Mi nombre es ", end="\n")
+print("9.Mi nombre es ", end="\n")  # \n salto de linea
 print("Monty Python.")
 
 print("10.Mi", "nombre", "es", "Monty", "Python.", sep="-") #separacion de c/argumento con -
@@ -48,4 +48,68 @@ print("10.Mi", "nombre", "es", "Monty", "Python.", sep="-") #separacion de c/arg
 print("11.Mi", "nombre", "es", sep="_", end="*") # Palabra clave pueden mezclarse en una 
 print("Monty", "Python.", sep="*", end="*\n")    # invocación. #end=""(fin arg),sep="-"(entre argum)
                                                 
-                                                 
+print("LAB12. Programming","Essentials","in", sep="***", end="...") #sin \n no hay salto de linea
+print("Python")                                                 
+
+print ("LAB13")
+###################
+print("original version:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("with fewer 'print()' invocations:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("higher:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("doubled:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
+
+print("15.1\nMi\nnombre\nes\nBond.", end=" ")
+print("James Bond.")
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
---
+++ Visual/Cisco/Modulo 2/LiteralesdePython.py
@@ -0,0 +1,26 @@
+#---Comentario de Documentacion---
+#CISCO_PYTHON 1
+#Programa: "Primeros pasos de Python"
+#@author: Ribas Carlos
+#@version 1.0.2
+#@see: """
+
+#---Comentario de bloque--- (este coment. no se visualiza en documentacion)
+# Realizando Seguimiento del Modulo 2 
+# Se busca identificar los diversos tipos de datos que se pueden utilizar.
+
+#---Comentario de Linea---
+#---NUMEROS LITERALES---
+print("2")  # se imprimira un caracter de tipo numerico (dos).
+#---NUMERROS ENTEROS
+print(2)    # se imprimira un valor numerico entero (dos).
+#---NUMEROS OCTALES Y HEXADECIMALES---
+print(0o123)    # 0o... es un nuemro octal. Se imrime su representacion en decimal.
+print(0o123)    # 0x... es un numero hexadecimal. 
+#---NUMEROS FLOTANTES---
+print(2.5)
+print(-0.4)
+#--- NUMERO CON POTENCIA
+print(3e8)
+#--- CODIFICANDO FLOTANTE
+print(6.62607E-34)
